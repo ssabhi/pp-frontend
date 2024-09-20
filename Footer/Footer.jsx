ec5ca4ce1b23ffc3b9eee71603f37cc6bcca469f
Abhishek Sachdeva <abhisheksachdeva1995@gmail.com>
2024-09-21 04:42:58 +0530

    Add files via upload

---
+++ Footer/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './Footer.css'
+
+import footer_logo from '../Assets/logo_big.png'
+import instagram_icon from '../Assets/instagram_icon.png'
+import pintrest_icon from '../Assets/pintester_icon.png'
+import whatsapp_icon from '../Assets/whatsapp_icon.png'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className="footer-logo">
+        <img src={footer_logo} alt="" />
+        <p>Premium Picks</p>
+      </div>
+      <ul className="footer-links">
+        <li>Company</li>
+        <li>Products</li>
+        <li>Offices</li>
+        <li>About</li>
+        <li><a href="tel:+919253131518">Contact</a></li>
+      </ul>
+      <div className="footer-social-icons">
+        <div className="footer-icons-container">
+            <img src={instagram_icon} alt="" />
+        </div>
+        <div className="footer-icons-container">
+            <img src={pintrest_icon} alt="" />
+        </div>
+        <div className="footer-icons-container">
+            <img src={whatsapp_icon} alt="" />
+            {/* <a href="https://api.whatsapp.com/send?phone=+919876543210">
+              <img src="whatsapp_icon.png" alt="WhatsApp"/>
+            </a> */}
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <hr />
+        <p>Copyright @ 2024 - All Right Reserved.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
